a92675bfb88db4115266bc1505963e0cbd74eb3c
Kursywa <mateusz.roszczyk@student.put.poznan.pl>
2023-06-05 14:05:28 +0200

    dodanie klasy Card i Deck

---
+++ Projekt10/Card.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt10
+{
+    internal class Card
+    {
+
+        string Suit = ""; //figura karty
+        int Value = 0; // wartosc karty
+        string FaceCard = ""; //widoczna figura
+        
+        public Card() //konstruktor bezparametrowy
+        {
+            Suit = "";
+            Value = 0;
+            FaceCard = "";
+        }
+
+        public Card(string s, int v) //konstruktor z konkretnymi wartościami
+        {
+            Suit = s;
+            Value = v;
+            switch (Value) //przypisanie figury w zależności od wartości
+            {
+                case 11:
+                    FaceCard = "Jack";
+                    break;
+                case 12:
+                    FaceCard = "Queen";
+                    break;
+                case 13:
+                    FaceCard = "King";
+                    break;
+                case 14:
+                    FaceCard = "Ace";
+                    break;
+                default:
+                    FaceCard = Value.ToString();
+                    break;
+            }
+        }
+        public void ChangeValue(int v) //metoda do zmiany wartości karty
+                    {
+                        Value = v;
+                    }
+    }
+}
---
+++ Projekt10/Deck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt10
+{
+    internal class Deck
+    {
+        List<Card> CurrentDeck;
+        int SizeOfDeck;
+
+        public Deck() //konstruktor tworzący standardową talię 52 kart
+        {
+            SizeOfDeck = 52;
+            CurrentDeck = new List<Card>(SizeOfDeck);
+        }
+
+        public void MakeDefaultDeck()
+        {
+            Card newCard;
+            for(int i =0; i < 52; i++)
+            {
+                if (i < 14)
+                {
+                    newCard = new Card("<3", i + 1); //serca to <3
+                }
+                else if (i < 27)
+                {
+                    newCard = new Card(">3", i + 1); // pik to >3
+                }
+                else if(i < 40)
+                {
+                    newCard = new Card("<>", i + 1); //karo to <>
+                }
+                else
+                {
+                    newCard = new Card("%", i + 1); // trefl to %
+                }
+
+                    CurrentDeck.Add(newCard);  //tak stworzona talia bedzie miala wszytskie figury
+                                               // z odpowiednimi kolorami
+            }
+        }
+
+
+    }
+}
