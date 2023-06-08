923edc184e655a0f30f1fcb629aa83f1d4e3f81f
Daniel Wiechetek 151581 <danwiechetek@gmail.com>
2023-06-08 18:48:48 +0200

    MainForm.cs

--- Projekt10/MainForm.cs
+++ Projekt10/MainForm.cs
@@ -22,7 +22,7 @@ namespace Projekt10
         private bool warwin;
         private bool blackjackwin;
         string[] Suits = { "hearts", "spades", "diamonds", "clubs" };
-        List<Card> PlayerHand, OpponentHand, MainDeck;
+        List<Card> PlayerDeck, OpponentDeck, MainDeck, PlayerSide, OpponentSide;
         int min, max;
 
         public MainForm()
@@ -47,23 +47,54 @@ namespace Projekt10
             warReset();
             Deck deck = new Deck();
             MainDeck = new List<Card>();
+            OpponentSide = new List<Card>();
+            PlayerSide = new List<Card>();
             deck.MakeDefaultDeck();
             MainDeck = deck.GetDeck();
             deck.Shuffle(MainDeck);//tosujemy karty
-            OpponentHand = new List<Card>(26);
-            PlayerHand = new List<Card>(26);
+            OpponentDeck = new List<Card>(26);
+            PlayerDeck = new List<Card>(26);
             for (int i = 0; i < 26; i++)
             {
-                OpponentHand.Add(deck.DrawCard(MainDeck));
-                PlayerHand.Add(deck.DrawCard(MainDeck));
+                OpponentDeck.Add(deck.DrawCard(MainDeck));
+                PlayerDeck.Add(deck.DrawCard(MainDeck));
             }
 
             while(warwin == false)
             {
+                if(DrawBtn.Enabled)//nie wiem chyba
+                {
+                    PlayerSide.Add(PlayerDeck[0]);
+                    PlayerDeck.RemoveAt(0);
 
+                    //tutaj wyświetlamy kartę gracza
+                }
+
+                OpponentSide.Add(PlayerDeck[0]);
+                OpponentDeck.RemoveAt(0);
+
+                WarComparison(PlayerSide[PlayerSide.Count - 1], OpponentSide[OpponentSide.Count - 1]);
             }
         }
 
+        private void WarComparison(Card playercard, Card opponentcard)
+        {
+            if (playercard.GetValue() == opponentcard.GetValue())
+            {
+                PlayerSide.Add(PlayerDeck[0]);
+                PlayerDeck.RemoveAt(0);
+                OpponentSide.Add(OpponentDeck[0]);
+                OpponentDeck.RemoveAt(0);
+            }
+            else if (playercard.GetValue() > opponentcard.GetValue()) 
+            {
+                PlayerDeck.AddRange(PlayerSide);
+            }
+            else
+            {
+                OpponentDeck.AddRange(OpponentSide);
+            }
+        }
 
         private void PlayBlackjack() //implementujemy oczko tutaj
         {
